453119ea64ffec0be4ebce3fa2db545c55a53565
ingridsimoes <isfurtado@hotmail.com>
2024-03-24 22:14:44 +0100

    TP4 - version 30

--- src/main/java/fr/univavignon/pokedex/api/IPokedexFactoryImpl.java
+++ src/main/java/fr/univavignon/pokedex/api/IPokedexFactoryImpl.java
@@ -11,6 +11,9 @@ public class IPokedexFactoryImpl implements IPokedexFactory {
      */
     @Override
     public IPokedex createPokedex(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory) {
+        if (metadataProvider == null || pokemonFactory == null) {
+            throw new IllegalArgumentException("Les paramètres ne peuvent pas être nuls.");
+        }
         return new IPokedexImpl(metadataProvider, pokemonFactory);
     }
 }
